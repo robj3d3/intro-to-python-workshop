1e886d5a2438b824cfa01f0e545b61b2f79d93a8
Robert Hallam <roboffice100@gmail.com>
2020-07-31 01:22:29 +0100

    chore(libraries) Added files

---
+++ libraries.py
@@ -0,0 +1,43 @@
+## STEP 1 ##
+
+# import turtle
+
+# t = turtle.Turtle()
+
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+
+
+## STEP 2 ##
+
+# import turtle
+
+# t = turtle.Turtle()
+
+# def forwardRight():
+#     t.forward(100)
+#     t.right(90)
+
+# forwardRight()
+# forwardRight()
+# forwardRight()
+# forwardRight()
+
+## STEP 3 ## 
+
+# import turtle
+
+# t = turtle.Turtle()
+
+# def forwardRight():
+#     t.forward(100)
+#     t.right(90)
+
+# for i in range(4):
+#     print(i)
+#     forwardRight()
