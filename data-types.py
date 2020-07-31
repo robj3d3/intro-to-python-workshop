0fa58015c6a6afbb2587cb7f3269d87ec2676fa4
Robert Hallam <roboffice100@gmail.com>
2020-07-31 01:20:01 +0100

    chore(data-types) Added files

---
+++ data-types.py
@@ -0,0 +1,19 @@
+## STEP 1 ##
+
+numberOne = 5
+numberTwo = 6
+
+print(numberOne + numberTwo)
+
+## STEP 2 ##
+
+# floatOne = 5.05
+# floatTwo = 6.001
+
+# print(floatOne + floatTwo)
+
+## STEP 3 ##
+
+# isCool = True
+
+# print("Am I cool? The answer is: {}").format(isCool) # str(isCool) and int(isCool)
